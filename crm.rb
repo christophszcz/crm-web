fcf271ebe93498f5a89da2ae621796342ca1c7b3
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-01-27 17:49:20 -0500

    Submit button is working- commit

--- crm.rb
+++ crm.rb
@@ -1,5 +1,8 @@
 require_relative './contact'
+require_relative './rolodex'
 require 'sinatra'
+
+$rolodex= Rolodex.new
 
 get '/' do
 	@crm_app_name = "My CRM"
@@ -15,10 +18,6 @@ get '/contacts/new' do
 end
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
+ 
   erb :contacts
 end
