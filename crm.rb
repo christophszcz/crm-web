c6cd64043f4a2a136cf6a76a0114e01ac6d63855
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-01-27 18:37:54 -0500

    Commit:HTTP Requests and Responses essentials working

--- crm.rb
+++ crm.rb
@@ -1,5 +1,5 @@
-require_relative './contact'
-require_relative './rolodex'
+require_relative 'contact'
+require_relative 'rolodex'
 require 'sinatra'
 
 $rolodex= Rolodex.new
@@ -17,7 +17,9 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-get "/contacts" do
- 
-  erb :contacts
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
 end
+
