c3af868716aed797000fd0ba7c9d0675fa296a8e
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-01-28 16:37:00 -0500

    Step 2-Add a header, footer, and navigation

--- crm.rb
+++ crm.rb
@@ -6,14 +6,17 @@ $rolodex= Rolodex.new
 
 get '/' do
 	@crm_app_name = "My CRM"
+	@title = "Sinatra"
   erb :index
 end
 
 get '/contacts' do
+  @title = "View All Contacts"
   erb :contacts
 end
 
 get '/contacts/new' do
+	@title =  "New Contact"
   erb :new_contact
 end
 
